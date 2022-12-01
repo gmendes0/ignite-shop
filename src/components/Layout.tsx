f25703dcf1e3b4ab22c4509eb4d5d9a3fc2eb8cf
Gabriel Mendes <gmendes230@gmail.com>
2022-12-01 01:29:04 -0300

    feat: next/image

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,8 @@
+import Image from "next/image";
 import { ReactNode } from "react";
 
 import logoSVG from "../assets/logo.svg";
-import { Container, Header } from "../styles/pages/app";
+import { Header } from "../styles/pages/app";
 
 interface LayoutProps {
   children: ReactNode;
@@ -11,7 +12,24 @@ export function Layout(props: LayoutProps) {
   return (
     <>
       <Header>
-        <img src={logoSVG.src} alt="" />
+        {/**
+         * Otmiza a imagem automaticamente
+         *
+         * Por exemplo: se temos uma imagem salva com o tamanho de 1080px de largura, mas
+         * ela vai ser colocada na tela em um container de 256px, não é necessário
+         * carregar a imagem com o tamanho original de 1080px. Ou caso a imagem que esta
+         * salva está no formato png mas ela nao possui transparencia, ou seja, seria melhor
+         * utilizar jpg que é mais leve.
+         *
+         * O componente Image consegue:
+         *  - Criar uma versão da imagem com o tamanho que a gente vai utilizar e usar essa
+         *    versão no lugar da original
+         *  - Converter o formato da imagem
+         *  - Cria imagem menores para dispositivos móveis, já que no mobile nao faz sentido
+         *    carregar uma imagem gigante para diminuir depois
+         */}
+        {/* <img src={logoSVG.src} alt="" /> */}
+        <Image src={logoSVG} alt="" />
       </Header>
 
       {props.children}
